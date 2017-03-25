77d58bfaa5f300834917191918947ed984037352
felipepetrin <felipepetrinmoura@gmail.com>
2017-03-25 18:03:32 -0300

    Adicionando gitignore

--- lesson1.py
+++ lesson1.py
@@ -3,7 +3,21 @@ import sys
 
 def solution(argv):
     binary = "{0:b}".format(argv)
-    binary_list = list()
+    binary_list = list(binary)
+    last_gap = 0
+    current_gap = 0
+
+    for num in binary_list:
+        if num == '1':
+            if current_gap > last_gap:
+                last_gap = current_gap
+            current_gap = 0
+        else:
+            current_gap += 1
+
+    return last_gap
+
 
 if __name__ == "__main__":
-    solution(int(sys.argv[1]))
+    gap = solution(int(sys.argv[1]))
+    print(gap)
